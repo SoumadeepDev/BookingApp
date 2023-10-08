e44fb579879a772910161cdb0576ac3baa38e5b4
SoumadeepDev <chat.soumadeep@gmail.com>
2023-10-08 23:32:46 +0530

    Adding Delete Button for UI and LocalStorage

--- script.js
+++ script.js
@@ -23,15 +23,52 @@ function displayAllUserData() {
         `;
 
     userDataList.forEach((userData, index) => {
-      bookingData.innerHTML += `
-                <div class="user-entry">
-                    <p><strong>Name:</strong> ${userData.name}</p>
-                    <p><strong>Email:</strong> ${userData.email}</p>
-                    <p><strong>Phone Number:</strong> ${userData.phone}</p>
-                </div>
+      const userEntry = document.createElement("div");
+      userEntry.classList.add("user-entry");
+      userEntry.innerHTML = `
+                <p><strong>Name:</strong> ${userData.name}</p>
+                <p><strong>Email:</strong> ${userData.email}</p>
+                <p><strong>Phone Number:</strong> ${userData.phone}</p>
+                <button class="delete-button" data-index="${index}">Delete</button>
             `;
+
+      bookingData.appendChild(userEntry);
+    });
+
+    // Add event listeners to delete buttons
+    const deleteButtons = document.querySelectorAll(".delete-button");
+    deleteButtons.forEach((button) => {
+      button.addEventListener("click", function (e) {
+        const indexToDelete = e.target.getAttribute("data-index");
+        deleteUserByIndex(indexToDelete);
+      });
     });
   }
+}
+
+// Function to delete a user by index
+function deleteUserByIndex(index) {
+  let userDataList = JSON.parse(localStorage.getItem("userDataList")) || [];
+
+  if (index >= 0 && index < userDataList.length) {
+    userDataList.splice(index, 1);
+    localStorage.setItem("userDataList", JSON.stringify(userDataList));
+    // Call checkUI to refresh the UI after removing a user
+    checkUI();
+  }
+}
+
+// Function to check the UI against local storage data and update the UI
+function checkUI() {
+  const userDataList = JSON.parse(localStorage.getItem("userDataList"));
+  const userEntries = document.querySelectorAll(".user-entry");
+
+  // Remove any user entries from the UI that do not exist in local storage
+  userEntries.forEach((userEntry, index) => {
+    if (!userDataList || index >= userDataList.length) {
+      userEntry.remove();
+    }
+  });
 }
 
 // Event listener for form submission
